ea91ecdad591b9869453d07281a910e298a49efe
Hauck <kolton.hauck@cotiviti.com>
2024-02-12 14:43:50 -0700

    testing errors

--- streamlit_test.py
+++ streamlit_test.py
@@ -17,22 +17,19 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 def st_messages_to_lc_messages(st_messages):
     lc_messages = []
     for message in st_messages:
-        try:
-            if message["role"] == "user":
-                lc_messages.append(
-                    HumanMessage(content=message["content"])
-                )
-            elif message["role"] == "assistant":
-                lc_messages.append(
-                    AIMessage(content=message["content"])
-                )
-            elif message["role"] == "system":
-                lc_messages.append(
-                    SystemMessage(content=message["content"])
-                )
-        except:
-            st.write(f"Current message failed: {message}")
-            st.write(f"message history: {st_messages}")
+        if message["role"] == "user":
+            lc_messages.append(
+                HumanMessage(content=message["content"])
+            )
+        elif message["role"] == "assistant":
+            lc_messages.append(
+                AIMessage(content=message["content"])
+            )
+        elif message["role"] == "system":
+            lc_messages.append(
+                SystemMessage(content=message["content"])
+            )
+
     return lc_messages
 
 
@@ -63,16 +60,6 @@ if prompt := st.chat_input("What is up?"):
         if not openai_api_key.startswith('sk-'):
             st.warning('Please enter your OpenAI API key!', icon='⚠')
 
-        #stream = get_client().chat.completions.create(
-        #stream = get_client()(
-        #    model=st.session_state["openai_model"],
-        #    messages=[
-        #        {"role": m["role"], "content": m["content"]}
-        #        for m in st.session_state.messages
-        #    ],
-        #    stream=True,
-        #)
-
         stream = get_client()(
             model=st.session_state["openai_model"],
             messages = st_messages_to_lc_messages(st.session_state.messages),
@@ -80,12 +67,8 @@ if prompt := st.chat_input("What is up?"):
         )    
         
         try:
-            response = st.write_stream(stream.content)
+            st.write_stream(stream.content)
         except:
-            response = st.write(stream.content)
+            st.write(stream.content)
 
-        st.write(f"response....{stream.content}")
         st.session_state.messages.append({"role": "assistant", "content": stream.content})
-
-    st.write("----- Message History -----")
-    st.write(st.session_state.messages)
